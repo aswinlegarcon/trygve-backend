d7d2ae933dc4c94c2399d9f6cada967e5ac58470
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-07-11 22:40:54 +0530

    Initial commit

---
+++ src/main/java/com/trygve_backend/trygve/TrygveApplication.java
@@ -0,0 +1,13 @@
+package com.trygve_backend.trygve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrygveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrygveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trygve_backend/trygve/TrygveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trygve_backend.trygve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrygveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
